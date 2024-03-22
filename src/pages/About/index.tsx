7d618845e46af274f6718dd6354d9c6f7d971536
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-03-22 15:32:43 +0100

    Added .env.example

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,20 +1,9 @@
-import { useQuery } from 'react-query';
 import Layout from 'components/Layout/Layout';
-import { http } from 'http/client';
 
 const About = () => {
-  const query = useQuery('posts', async () => {
-    return await http.get('/api/posts');
-  });
-
   return (
     <Layout>
       <div>About</div>
-      {query.isLoading ? (
-        <div>Loading</div>
-      ) : (
-        <code>{JSON.stringify(query.data, null, 2)}</code>
-      )}
     </Layout>
   );
 };
