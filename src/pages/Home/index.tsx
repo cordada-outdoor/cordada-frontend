27df262b86571d3c3b84cc04c82f92e35638646a
Joan Bogunya Salvans <joanbogunya98@gmail.com>
2024-04-02 16:35:55 +0200

    home page finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Typography } from "@mui/material"
+import { Box, Button, Typography, useMediaQuery, useTheme } from "@mui/material"
 import Layout from "components/Layout/Layout"
 import HomeBg from "assets/images/home_bg.jpg"
+import AboutImg from "assets/images/about-us.jpg"
 import LogoWhite from "assets/logos/logo-big-white.png";
 import "./index.scss"
 import { useEffect, useRef, useState } from "react";
@@ -14,6 +15,8 @@ const Home = () => {
     const projectsRef = useRef();
     const [projectsSectionVisible, updateProjectsSectionVisible] = useState<boolean>(false);
     const { langUrlPrefix } = useUrlLang();
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
     useEffect(() => {
         const observer = new IntersectionObserver((entries, observer) => {
@@ -62,7 +65,7 @@ const Home = () => {
                         dots: false,
                         arrows: false,
                         infinite: true,
-                        slidesToShow: 6,
+                        slidesToShow: isMobile ? 3 : 6,
                         slidesToScroll: 1,
                         autoplay: true,
                         autoplaySpeed: 7000,
@@ -78,33 +81,31 @@ const Home = () => {
                     </Carousel>
                 </Box>
             </Box>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            <Box className="home-services-section">
+                <Typography variant="h3">{t("homePage.ourServices")}</Typography>
+                <Box py={2} />
+                <Typography>{t("homePage.servicesText1")}</Typography>
+                <Box py={2} />
+                <Typography variant="h5">{t("homePage.expertsIn")}</Typography>
+                <Box mt={1} />
+                <Typography variant="h4">{t("homePage.expertsDescription")}</Typography>
+                <Link className="see-services-button-container" to={`${langUrlPrefix + '/services'}`}>
+                    <Button className="see-services-button" color="primary" variant="contained">{t("homePage.allServices")}</Button>
+                </Link>
+            </Box>
+            <Box className="home-about-section">
+                <Box className="about-us-img-container">
+                    <img src={AboutImg} alt="about-us" className="about-us-img" />
+                    <Link className="about-us-button-container" to={`${langUrlPrefix + '/about'}`}>
+                        <Button className="about-us-button" color="primary" variant="contained">{t("aboutUs")}</Button>
+                    </Link>
+                </Box>
+
+                <Box className="home-about-contact-us-section">
+                    <Typography variant="h3">{t("homePage.contactUs")}</Typography>
+                    <a title="Email" className="contact-us-button" href="mailto:info@cordadaoutdoor.com" />
+                </Box>
+            </Box>
         </Box>
     </Layout>
 }
