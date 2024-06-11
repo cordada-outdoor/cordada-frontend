dfb156adbfecc3bf4ecc121095e475734b9f235b
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-06-11 16:46:37 +0200

    Added single project route

--- src/utils/Routes.tsx
+++ src/utils/Routes.tsx
@@ -1,5 +1,5 @@
-import Home from "pages/Home"
-import { Redirect, Route, Switch } from "react-router-dom"
+import Home from "pages/Home";
+import { Redirect, Route, Switch } from "react-router-dom";
 import useUrlLang from "./useUrlLang";
 import About from "pages/About";
 import NotFound from "pages/NotFound";
@@ -7,45 +7,50 @@ import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import Services from "pages/Services";
 import Projects from "pages/Projects";
+import SingleProject from "pages/SingleProject";
 
 const Routes = () => {
-    const { langUrlPrefix, path, lang } = useUrlLang();
-    const pathPrefix = path.replace(/\/$/, "");
-    const { i18n } = useTranslation()
+  const { langUrlPrefix, path, lang } = useUrlLang();
+  const pathPrefix = path.replace(/\/$/, "");
+  const { i18n } = useTranslation();
 
-    useEffect(() => {
-        if (lang !== i18n.language) {
-            i18n.changeLanguage(lang)
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [lang])
+  useEffect(() => {
+    if (lang !== i18n.language) {
+      i18n.changeLanguage(lang);
+    }
+  }, [i18n, lang]);
 
-    return <Switch>
-        <Route exact path={path}>
-            <Home />
-        </Route>
-        <Route exact path={`${pathPrefix}/about`}>
-            <About />
-        </Route>
-        <Route path={`${pathPrefix}/about`}>
-            <Redirect to={`${langUrlPrefix}/about`} />
-        </Route>
-        <Route exact path={`${pathPrefix}/services`}>
-            <Services />
-        </Route>
-        <Route path={`${pathPrefix}/services`}>
-            <Redirect to={`${langUrlPrefix}/services`} />
-        </Route>
-        <Route exact path={`${pathPrefix}/projects`}>
-            <Projects />
-        </Route>
-        <Route path={`${pathPrefix}/projects`}>
-            <Redirect to={`${langUrlPrefix}/projects`} />
-        </Route>
-        <Route path={'*'}>
-            <NotFound />
-        </Route>
+  return (
+    <Switch>
+      <Route exact path={path}>
+        <Home />
+      </Route>
+      <Route exact path={`${pathPrefix}/about`}>
+        <About />
+      </Route>
+      <Route path={`${pathPrefix}/about`}>
+        <Redirect to={`${langUrlPrefix}/about`} />
+      </Route>
+      <Route exact path={`${pathPrefix}/services`}>
+        <Services />
+      </Route>
+      <Route path={`${pathPrefix}/services`}>
+        <Redirect to={`${langUrlPrefix}/services`} />
+      </Route>
+      <Route exact path={`${pathPrefix}/project/:id`}>
+        <SingleProject />
+      </Route>
+      <Route exact path={`${pathPrefix}/projects`}>
+        <Projects />
+      </Route>
+      <Route path={`${pathPrefix}/projects`}>
+        <Redirect to={`${langUrlPrefix}/projects`} />
+      </Route>
+      <Route path={"*"}>
+        <NotFound />
+      </Route>
     </Switch>
-}
+  );
+};
 
 export default Routes;
