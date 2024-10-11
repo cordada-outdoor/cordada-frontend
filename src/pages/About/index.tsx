e00171674af1f052243be8bdce91376bd8d70489
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-10-11 11:03:51 +0200

    Fetching data from CMS (#6)

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,45 +1,54 @@
 import { Box, Typography } from "@mui/material";
-import Layout from "components/Layout/Layout"
-import { useTranslation } from "react-i18next";
-import HeaderImg from 'assets/images/about-us-top-img.png';
-import './index.scss';
+import Layout from "components/Layout/Layout";
+import HeaderImg from "assets/images/about-us-top-img.png";
+import "./index.scss";
 import AboutUsDescription from "components/AboutUsDescription";
+import { useQuery } from "@tanstack/react-query";
+import { http } from "http/client";
 
 const About = () => {
-  const { t } = useTranslation();
+  const { data, isLoading } = useQuery({
+    queryKey: ["about-us-page"],
+    queryFn: async () => {
+      const res = await http.get("/api/about-us-page", {
+        params: {
+          "populate[sections][populate]": "*",
+        },
+      });
+
+      return res.data;
+    },
+  });
+
+  if (isLoading) return <h1>Loading</h1>;
+
+  const content = data.data.attributes;
+
+  // eslint-disable-next-line no-console
+  console.log({ content });
+
   return (
     <Layout>
       <Box className="about">
-        <Typography variant="h3">{t("aboutUs").toLowerCase()}</Typography>
-
+        <Typography variant="h3">{content.header}</Typography>
         <Box className="about-top-image-container">
           <img src={HeaderImg} alt="header-img" className="about-top-image" />
         </Box>
         <Box className="about-us-content">
-          <AboutUsDescription title="JOSEP MORALES" direction="left-to-right" image={HeaderImg} children={(
-            <Box>
-              <Typography fontWeight={300}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-              <Typography fontWeight={300}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-              <br />
-              <Typography>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-            </Box>
-          )} />
-          <AboutUsDescription title="JOSEP MORALES" direction="right-to-left" image={HeaderImg} children={(
-            <Box>
-              <Typography fontWeight={300}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-              <Typography fontWeight={300}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-              <br />
-              <Typography>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-            </Box>
-          )} />
-          <AboutUsDescription title="JOSEP MORALES" direction="left-to-right" image={HeaderImg} children={(
-            <Box>
-              <Typography fontWeight={300}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-              <Typography fontWeight={300}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-              <br />
-              <Typography>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-            </Box>
-          )} />
+          {content.sections.map((sec: any) => {
+            return (
+              <AboutUsDescription
+                title={sec.name}
+                direction="left-to-right"
+                image={HeaderImg}
+                children={
+                  <Box>
+                    <Typography fontWeight={300}>{sec.body}</Typography>
+                  </Box>
+                }
+              />
+            );
+          })}
         </Box>
       </Box>
     </Layout>
