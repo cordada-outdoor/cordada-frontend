20a9675bf3373f57b11063831dc3823cb93e4b78
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-11-12 20:05:11 +0100

    Improved query hook usage

--- src/pages/SingleProject/index.tsx
+++ src/pages/SingleProject/index.tsx
@@ -14,6 +14,7 @@ import {
   useTheme,
 } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
+import dayjs from "dayjs";
 
 import HomeBg from "assets/images/home_bg.jpg";
 import ClientInProject from "components/ClientInProject";
@@ -23,11 +24,14 @@ import { http } from "http/client";
 import { Client } from "models/client";
 import { Project } from "models/project";
 import { Service } from "models/service";
-import { formatDate, getImageUrl } from "utils";
+import { getImageUrl } from "utils";
 
 const SingleProject = () => {
   const { t } = useTranslation();
   const [imgModal, setImgModal] = useState<boolean>(false);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   const { id } = useParams<{
     id: string;
@@ -46,103 +50,104 @@ const SingleProject = () => {
     },
   });
 
-  const project: Project = projectQuery?.data?.data;
+  if (projectQuery.isLoading) {
+    return (
+      <Layout>
+        <CircularProgress sx={{ color: "#ff521c" }} />
+      </Layout>
+    );
+  }
 
-  const client: Client | undefined = project?.attributes?.client?.data;
-  const services: Service[] = project?.attributes?.services?.data ?? [];
-  const image = project?.attributes?.image;
+  const project: Project = projectQuery.data.data;
+  const client: Client = project.attributes.client.data;
+  const services: Service[] = project.attributes.services.data;
+
+  const image = project.attributes.image;
   const imgUrl = getImageUrl(image, "large");
-  const projectDate = formatDate(project?.attributes?.date ?? "");
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const projectDate = dayjs(project.attributes.date).format("MM/YYYY");
 
   const dataVariant = isMobile ? "h6" : "h4";
   return (
     <Layout>
       <Box className="project-detail">
-        {projectQuery.isLoading || !project ? (
-          <CircularProgress sx={{ color: "#ff521c" }} />
-        ) : (
-          <>
-            <Typography className="project-title" variant="h3">
-              {project.attributes?.title ?? "TBD"}
-            </Typography>
-            <Box
-              className="project-img-container"
-              onClick={() => setImgModal(!imgModal)}
-            >
-              <img src={imgUrl ?? HomeBg} alt="project-header" />
-            </Box>
-            <Grid container>
-              <Grid item md={4} xs={12} className="project-data">
-                <table>
-                  <tr className="project-data-element">
-                    <td>
-                      <Typography fontWeight={300} variant={dataVariant}>
-                        {t("projectsPage.client")}
-                      </Typography>
-                    </td>
-                    <td>
-                      <Typography
-                        fontWeight={700}
-                        variant={dataVariant}
-                        className="project-data-primary"
-                      >
-                        {client?.attributes?.name ?? "N/A"}
-                      </Typography>
-                    </td>
-                  </tr>
-                  <tr className="project-data-element">
-                    <td>
-                      <Typography fontWeight={300} variant={dataVariant}>
-                        {t("projectsPage.date")}
-                      </Typography>
-                    </td>
-                    <td>
-                      <Typography
-                        fontWeight={700}
-                        variant={dataVariant}
-                        className="project-data-primary"
-                      >
-                        {projectDate}
-                      </Typography>
-                    </td>
-                  </tr>
-                  <tr className="project-data-element">
-                    <td>
-                      <Typography fontWeight={300} variant={dataVariant}>
-                        {t("projectsPage.type")}
-                      </Typography>
-                    </td>
-                    <td>
-                      <Typography
-                        fontWeight={700}
-                        variant={dataVariant}
-                        className="project-data-primary"
-                      >
-                        {services
-                          .map((s) => {
-                            return s.attributes.name;
-                          })
-                          .join(" & ")}
-                      </Typography>
-                    </td>
-                  </tr>
-                </table>
-              </Grid>
-              <Grid
-                item
-                md={8}
-                xs={12}
-                sx={{ fontWeight: 300 }}
-                className="project-body markdown-container"
-              >
-                <RenderMarkdown markdown={project?.attributes?.body} />
-              </Grid>
-            </Grid>
-            {client?.id && <ClientInProject id={Number(client.id)} />}
-          </>
-        )}
+        <Typography className="project-title" variant="h3">
+          {project.attributes.title}
+        </Typography>
+        <Box
+          className="project-img-container"
+          onClick={() => setImgModal(!imgModal)}
+        >
+          <img src={imgUrl ?? HomeBg} alt="project-header" />
+        </Box>
+        <Grid container>
+          <Grid item md={4} xs={12} className="project-data">
+            <table>
+              <tr className="project-data-element">
+                <td>
+                  <Typography fontWeight={300} variant={dataVariant}>
+                    {t("projectsPage.client")}
+                  </Typography>
+                </td>
+                <td>
+                  <Typography
+                    fontWeight={700}
+                    variant={dataVariant}
+                    className="project-data-primary"
+                  >
+                    {client.attributes.name}
+                  </Typography>
+                </td>
+              </tr>
+              <tr className="project-data-element">
+                <td>
+                  <Typography fontWeight={300} variant={dataVariant}>
+                    {t("projectsPage.date")}
+                  </Typography>
+                </td>
+                <td>
+                  <Typography
+                    fontWeight={700}
+                    variant={dataVariant}
+                    className="project-data-primary"
+                  >
+                    {projectDate}
+                  </Typography>
+                </td>
+              </tr>
+              <tr className="project-data-element">
+                <td>
+                  <Typography fontWeight={300} variant={dataVariant}>
+                    {t("projectsPage.type")}
+                  </Typography>
+                </td>
+                <td>
+                  <Typography
+                    fontWeight={700}
+                    variant={dataVariant}
+                    className="project-data-primary"
+                  >
+                    {services
+                      .map((s) => {
+                        return s.attributes.name;
+                      })
+                      .join(" & ")}
+                  </Typography>
+                </td>
+              </tr>
+            </table>
+          </Grid>
+          <Grid
+            item
+            md={8}
+            xs={12}
+            sx={{ fontWeight: 300 }}
+            className="project-body markdown-container"
+          >
+            <RenderMarkdown markdown={project.attributes.body} />
+          </Grid>
+        </Grid>
+        {client?.id && <ClientInProject id={Number(client.id)} />}
       </Box>
       <Dialog open={imgModal} onClose={() => setImgModal(!imgModal)}>
         <img alt="full-project-img" src={imgUrl ?? HomeBg} />
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -17,19 +17,6 @@ export const isEven = (x: number) => {
   return x % 2 === 0;
 };
 
-export const formatDate = (date: string) => {
-  const projectDate = new Date(date ?? undefined);
-  const yyyy = projectDate.getFullYear();
-  const mm =
-    projectDate.getMonth() + 1 < 10
-      ? "0" + projectDate.getMonth()
-      : projectDate.getMonth();
-  //can add day if we want
-  // const dd = Number(projectDate.getDate()) < 10 ? '0' + projectDate.getDate() : projectDate.getDate();
-
-  return mm + "/" + yyyy;
-};
-
 export const useQueryParams = () => {
   const location = useLocation();
 
